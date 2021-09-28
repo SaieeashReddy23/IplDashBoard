734d3ff5fc026e1346432481cb60c3ec0c93b286
SaieeashReddy23 <saieeashreddy1@gmail.com>
2021-09-28 11:04:38 +0530

    Team Entity created with each team total matches and its wins

---
+++ IplDashBoard/src/main/java/com/sai/IplDashBoard/model/Team.java
@@ -0,0 +1,66 @@
+package com.sai.IplDashBoard.model;
+
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long teamId;
+    private String teamName;
+    private long totalMatches;
+    private long totalWins;
+
+    public Team(String teamName, long totalMatches) {
+        this.teamName = teamName;
+        this.totalMatches = totalMatches;
+    }
+
+    public long getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(long teamId) {
+        this.teamId = teamId;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public long getTotalMatches() {
+        return totalMatches;
+    }
+
+    public void setTotalMatches(long totalMatches) {
+        this.totalMatches = totalMatches;
+    }
+
+    public long getTotalWins() {
+        return totalWins;
+    }
+
+    public void setTotalWins(long totalWins) {
+        this.totalWins = totalWins;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "teamId=" + teamId +
+                ", teamName='" + teamName + '\'' +
+                ", totalMatches=" + totalMatches +
+                ", totalWins=" + totalWins +
+                '}';
+    }
+}
